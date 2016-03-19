dcfa6ca2ee2fecfbcc955486b85729c801c25a1e
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-03-19 14:43:15 +0100

    Reformat code

--- bbp/sequential/include/bbp_sequential.cpp
+++ bbp/sequential/include/bbp_sequential.cpp
@@ -1,125 +1,125 @@
 #include "bbp_sequential.h"
 
 float mila::bbp::sequential::BBP::ComputeDigit(size_t position) const {
-    auto series_1 = Series(1, position);
-    auto series_2 = Series(4, position);
-    auto series_3 = Series(5, position);
-    auto series_4 = Series(6, position);
-    auto result = 4.0f * series_1 - 2.0f * series_2 - series_3 - series_4;  // BBP formula
-    result = result - truncf(result) + 1.0f;
-    return result;
+  auto series_1 = Series(1, position);
+  auto series_2 = Series(4, position);
+  auto series_3 = Series(5, position);
+  auto series_4 = Series(6, position);
+  auto result = 4.0f * series_1 - 2.0f * series_2 - series_3 - series_4;  // BBP formula
+  result = result - truncf(result) + 1.0f;
+  return result;
 }
 
 float mila::bbp::sequential::BBP::Series(size_t j, size_t d) const {
-    auto sum = 0.0f;
+  auto sum = 0.0f;
 
-    for (size_t k = 0; k < d; ++k) {
-        auto ak = 8.0f * k + j;
-        auto p = d - k;
-        auto t = ModularExponentiation(16.0f, p, ak);
-        sum += t / ak;
-        sum -= truncf(sum);
+  for (size_t k = 0; k < d; ++k) {
+    auto ak = 8.0f * k + j;
+    auto p = d - k;
+    auto t = ModularExponentiation(16.0f, p, ak);
+    sum += t / ak;
+    sum -= truncf(sum);
+  }
+
+  for (size_t k = d; k <= d + 100; ++k) {
+    auto ak = 8.0f * k + j;
+    auto p = static_cast<float>(d) - static_cast<float>(k);
+    auto t = powf(16.0f, p) / ak;
+
+    if (t < precision_) {
+      break;
     }
 
-    for (size_t k = d; k <= d + 100; ++k) {
-        auto ak = 8.0f * k + j;
-        auto p = static_cast<float>(d) - static_cast<float>(k);
-        auto t = powf(16.0f, p) / ak;
+    sum += t;
+    sum -= truncf(sum);
+  }
 
-        if (t < precision_) {
-            break;
-        }
-
-        sum += t;
-        sum -= truncf(sum);
-    }
-
-    return sum;
+  return sum;
 }
 
 float mila::bbp::sequential::BBP::ModularExponentiation(float b, size_t e, float m) const {
-    if (m == 1.0f) {
-        return 0.0f;
+  if (m == 1.0f) {
+    return 0.0f;
+  }
+
+  auto c = 1.0f;
+  auto p = LargestPowerOfTwoLessOrEqual(e);
+  auto t = powf(2.0f, static_cast<float>(p - 1));
+
+  for (size_t i = 0; i < p; ++i) {
+    if (e >= t) {
+      c *= b;
+      c -= trunc(c / m) * m;
+      e -= t;
     }
 
-    auto c = 1.0f;
-    auto p = LargestPowerOfTwoLessOrEqual(e);
-    auto t = powf(2.0f, static_cast<float>(p - 1));
+    t *= 0.5f;
 
-    for (size_t i = 0; i < p; ++i) {
-        if (e >= t) {
-            c *= b;
-            c -= trunc(c / m) * m;
-            e -= t;
-        }
-
-        t *= 0.5f;
-
-        if (t >= 1.0f) {
-            c *= c;
-            c -= trunc(c / m) * m;
-        }
+    if (t >= 1.0f) {
+      c *= c;
+      c -= trunc(c / m) * m;
     }
+  }
 
-    return c;
+  return c;
 }
 
 size_t mila::bbp::sequential::BBP::LargestPowerOfTwoLessOrEqual(size_t n) const {
-    auto i = size_t{0};
-    for (i = 0; i < kPowersOfTwo.size(); ++i) {
-        if (kPowersOfTwo[i] > n) {
-            break;
-        }
+  auto i = size_t{0};
+  for (i = 0; i < kPowersOfTwo.size(); ++i) {
+    if (kPowersOfTwo[i] > n) {
+      break;
     }
-    return i;
+  }
+  return i;
 }
 
-mila::bbp::sequential::BBP::BBP(double precision): precision_(precision) {
+mila::bbp::sequential::BBP::BBP(double precision) : precision_(precision) {
 
 }
 
-mila::bbp::sequential::BBP::BBP(): precision_(1e-5) {
+mila::bbp::sequential::BBP::BBP() : precision_(1e-5) {
 
 }
 
 double mila::bbp::sequential::BBP::precision() const {
-    return precision_;
+  return precision_;
 }
 
 std::string mila::bbp::sequential::BBP::ConvertFractionToHex(float number, size_t number_of_digits) const {
-    const auto kHex = std::string("0123456789ABCDEF");
-    auto digit = std::string(number_of_digits, '-');
-    auto value = fabsf(number);
-    for (size_t i = 0; i < number_of_digits; ++i) {
-        value = 16.0f * (value - floorf(value));
-        digit[i] = kHex[static_cast<size_t>(value)];
-    }
-    return digit;
+  const auto kHex = std::string("0123456789ABCDEF");
+  auto digit = std::string(number_of_digits, '-');
+  auto value = fabsf(number);
+  for (size_t i = 0; i < number_of_digits; ++i) {
+    value = 16.0f * (value - floorf(value));
+    digit[i] = kHex[static_cast<size_t>(value)];
+  }
+  return digit;
 }
 
 std::vector<float> mila::bbp::sequential::BBP::ComputeDigits(size_t number_of_digits, size_t starting_position) const {
-    auto digits = std::vector<float>(number_of_digits, 0.0f);
-    for (size_t i = 0; i < digits.size(); ++i) {
-        digits[i] = ComputeDigit(starting_position + i);
-    }
-    return digits;
+  auto digits = std::vector<float>(number_of_digits, 0.0f);
+  for (size_t i = 0; i < digits.size(); ++i) {
+    digits[i] = ComputeDigit(starting_position + i);
+  }
+  return digits;
 }
 
 std::vector<std::string> mila::bbp::sequential::BBP::ConvertFractionsToHex(std::vector<float> numbers,
                                                                            size_t number_of_digits) const {
-    auto digits = std::vector<std::string>(numbers.size(), "-");
-    for (size_t i = 0; i < digits.size(); ++i) {
-        digits[i] = ConvertFractionToHex(numbers[i], number_of_digits);
-    }
-    return digits;
+  auto digits = std::vector<std::string>(numbers.size(), "-");
+  for (size_t i = 0; i < digits.size(); ++i) {
+    digits[i] = ConvertFractionToHex(numbers[i], number_of_digits);
+  }
+  return digits;
 }
 
 std::string mila::bbp::sequential::BBP::Run(size_t number_of_digits, size_t starting_position) const {
-    auto digits = ComputeDigits(number_of_digits, starting_position);
-    auto hex_digits = ConvertFractionsToHex(digits, 1);
-    auto output = std::string("");
-    for (auto digit : hex_digits) {
-        output += digit[0];
-    }
-    return output;
+  auto digits = ComputeDigits(number_of_digits, starting_position);
+  auto hex_digits = ConvertFractionsToHex(digits, 1);
+  auto output = std::string("");
+  for (auto digit : hex_digits) {
+    output += digit[0];
+  }
+  return output;
 }
--- bbp/sequential/include/bbp_sequential.h
+++ bbp/sequential/include/bbp_sequential.h
@@ -7,31 +7,31 @@
 #include <string>
 
 namespace mila {
-    namespace bbp {
-        namespace sequential {
-            const std::vector<size_t> kPowersOfTwo =  {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
-                                                       4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288,
-                                                       1048576, 2097152, 4194304, 8388608, 16777216, 33554432};
-            class BBP {
-            public:
-                BBP();
-                BBP(double precision);
+namespace bbp {
+namespace sequential {
+const std::vector<size_t> kPowersOfTwo = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
+                                          4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288,
+                                          1048576, 2097152, 4194304, 8388608, 16777216, 33554432};
+class BBP {
+ public:
+  BBP();
+  BBP(double precision);
 
-                float ComputeDigit(size_t position) const;
-                float Series(size_t j, size_t d) const;  // Compute sum_k 16^(d-k)/(8*k*j)
-                float ModularExponentiation(float b, size_t e, float m) const;  // Compute b^e mod m
-                size_t LargestPowerOfTwoLessOrEqual(size_t n) const;
-                std::string ConvertFractionToHex(float number, size_t number_of_digits)const ;
-                std::vector<std::string> ConvertFractionsToHex(std::vector<float> numbers, size_t number_of_digits) const;
-                std::vector<float> ComputeDigits(size_t number_of_digits, size_t starting_position) const;
-                std::string Run(size_t number_of_digits, size_t starting_position) const;
+  float ComputeDigit(size_t position) const;
+  float Series(size_t j, size_t d) const;  // Compute sum_k 16^(d-k)/(8*k*j)
+  float ModularExponentiation(float b, size_t e, float m) const;  // Compute b^e mod m
+  size_t LargestPowerOfTwoLessOrEqual(size_t n) const;
+  std::string ConvertFractionToHex(float number, size_t number_of_digits) const;
+  std::vector<std::string> ConvertFractionsToHex(std::vector<float> numbers, size_t number_of_digits) const;
+  std::vector<float> ComputeDigits(size_t number_of_digits, size_t starting_position) const;
+  std::string Run(size_t number_of_digits, size_t starting_position) const;
 
-                double precision() const;
-            private:
-                const double precision_;
-            };
-        };  // sequential
-    };  // bbp
+  double precision() const;
+ private:
+  const double precision_;
+};
+};  // sequential
+};  // bbp
 }  // mila
 
 #endif  // MILA_BBP_SEQUENTIAL_H_
