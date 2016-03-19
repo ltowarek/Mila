a21968e5510b7f06d1b70b2cd05673c2d441f730
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-03-19 14:57:12 +0100

    Store configuration in a structure

--- bbp/sequential/main.cpp
+++ bbp/sequential/main.cpp
@@ -2,13 +2,19 @@
 #include <string>
 #include "bbp_sequential.h"
 
+struct parameters {
+  size_t number_of_digits;
+  size_t starting_position;
+};
+
 int main() {
+  parameters config;
+  config.number_of_digits = 10;
+  config.starting_position = 10;
   mila::bbp::sequential::BBP bbp;
-  size_t number_of_digits = 10;
-  size_t starting_position = 10;
-  std::string output = bbp.Run(number_of_digits, starting_position);
-  printf("Number of Digits: %d\n", number_of_digits);
-  printf("Starting Position: %d\n", starting_position);
+  std::string output = bbp.Run(config.number_of_digits, config.starting_position);
+  printf("Number of Digits: %d\n", config.number_of_digits);
+  printf("Starting Position: %d\n", config.starting_position);
   printf("PI in hex: %s\n", output.c_str());
   return 0;
 }
