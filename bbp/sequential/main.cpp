b02bd4786f762b389686567c649915ecbea1ddf7
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-03-19 14:55:37 +0100

    Add simple program using sequential bbp

--- bbp/sequential/main.cpp
+++ bbp/sequential/main.cpp
@@ -1,3 +1,14 @@
+#include <cstdio>
+#include <string>
+#include "bbp_sequential.h"
+
 int main() {
-    return 0;
+  mila::bbp::sequential::BBP bbp;
+  size_t number_of_digits = 10;
+  size_t starting_position = 10;
+  std::string output = bbp.Run(number_of_digits, starting_position);
+  printf("Number of Digits: %d\n", number_of_digits);
+  printf("Starting Position: %d\n", starting_position);
+  printf("PI in hex: %s\n", output.c_str());
+  return 0;
 }
