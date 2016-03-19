30ce39b5fac0934a5c52e76fcbd470e8ba5ced00
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-03-19 15:14:59 +0100

    Parse command line arguments

--- bbp/sequential/main.cpp
+++ bbp/sequential/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <string>
 #include "bbp_sequential.h"
 
@@ -7,11 +8,17 @@ struct parameters {
   size_t starting_position;
 };
 
-int main() {
-  parameters config;
-  config.number_of_digits = 10;
-  config.starting_position = 10;
-  mila::bbp::sequential::BBP bbp;
+parameters ParseCommandLine(int argc, char** argv) {
+  auto config = parameters{0};
+  // TODO: Verify input and add help message
+  config.number_of_digits = static_cast<size_t>(atoi(argv[1]));
+  config.starting_position = static_cast<size_t>(atoi(argv[2]));
+  return config;
+}
+
+int main(int argc, char** argv) {
+  auto config = ParseCommandLine(argc, argv);
+  auto bbp = mila::bbp::sequential::BBP();
   std::string output = bbp.Run(config.number_of_digits, config.starting_position);
   printf("Number of Digits: %d\n", config.number_of_digits);
   printf("Starting Position: %d\n", config.starting_position);
