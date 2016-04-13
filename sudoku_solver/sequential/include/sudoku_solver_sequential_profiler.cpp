00fb108657fdef25be6f540774226d1853ebdd19
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-04-13 22:34:09 +0200

    Add SudokuSolverBasedOnFilesProfiler

--- sudoku_solver/sequential/include/sudoku_solver_sequential_profiler.cpp
+++ sudoku_solver/sequential/include/sudoku_solver_sequential_profiler.cpp
@@ -18,3 +18,31 @@ std::vector<int> mila::sudokusolver::sequential::SudokuSolverProfiler::Run(const
   results_.insert(std::pair<std::string, int64_t>("Run", duration));
   return output;
 }
+
+mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler::SudokuSolverBasedOnFilesProfiler(): main_result_("RunWithFiles") {}
+
+std::string mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler::main_result() const {
+  return main_result_;
+}
+
+std::map<std::string, int64_t> mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler::results() const {
+  return results_;
+}
+
+std::vector<int> mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler::Run(const std::vector<int> &grid, int n) {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  auto output = SudokuSolver::Run(grid, n);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  results_.insert(std::pair<std::string, int64_t>("RunWithoutFiles", duration));
+  return output;
+}
+
+void mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler::Run(const std::string &input_file_name,
+                                                                           const std::string &output_file_name) {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  SudokuSolverBasedOnFiles::Run(input_file_name, output_file_name);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  results_.insert(std::pair<std::string, int64_t>("RunWithFiles", duration));
+}
--- sudoku_solver/sequential/include/sudoku_solver_sequential_profiler.h
+++ sudoku_solver/sequential/include/sudoku_solver_sequential_profiler.h
@@ -23,6 +23,20 @@ class SudokuSolverProfiler: public SudokuSolver {
   std::map<std::string, int64_t> results_;
 };
 
+class SudokuSolverBasedOnFilesProfiler: public SudokuSolverBasedOnFiles {
+ public:
+  SudokuSolverBasedOnFilesProfiler();
+
+  std::vector<int> Run(const std::vector<int>& grid, int n) override;
+  void Run(const std::string& input_file_name, const std::string& output_file_name) override;
+
+  std::string main_result() const;
+  std::map<std::string, int64_t> results() const;
+ private:
+  const std::string main_result_;
+  std::map<std::string, int64_t> results_;
+};
+
 }  // sequential
 }  // sudokusolver
 }  // mila
