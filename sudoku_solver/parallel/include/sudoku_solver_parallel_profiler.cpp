9661a70a66c5015b16875066409a71a5f4c49c6f
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-04-16 16:50:44 +0200

    Add SudokuSolverProfiler class

--- sudoku_solver/parallel/include/sudoku_solver_parallel_profiler.cpp
+++ sudoku_solver/parallel/include/sudoku_solver_parallel_profiler.cpp
@@ -1,1 +1,25 @@
 #include "sudoku_solver_parallel_profiler.h"
+
+mila::sudokusolver::parallel::SudokuSolverProfiler::SudokuSolverProfiler(): SudokuSolverProfiler(0, 0) {
+}
+
+mila::sudokusolver::parallel::SudokuSolverProfiler::SudokuSolverProfiler(size_t platform_id, size_t device_id): SudokuSolver(platform_id, device_id), main_result_("Run") {
+}
+
+std::vector<int> mila::sudokusolver::parallel::SudokuSolverProfiler::Run(const std::vector<int> &grid,
+                                                                         int number_of_cells_to_fill) {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  auto output = SudokuSolver::Run(grid, number_of_cells_to_fill);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  results_.insert(std::pair<std::string, int64_t>("Run", duration));
+  return output;
+}
+
+std::string mila::sudokusolver::parallel::SudokuSolverProfiler::main_result() const {
+  return main_result_;
+}
+
+std::map<std::string, int64_t> mila::sudokusolver::parallel::SudokuSolverProfiler::results() const {
+  return results_;
+}
--- sudoku_solver/parallel/include/sudoku_solver_parallel_profiler.h
+++ sudoku_solver/parallel/include/sudoku_solver_parallel_profiler.h
@@ -1,4 +1,31 @@
 #ifndef MILA_SUDOKU_SOLVER_PARALLEL_PROFILER_H_
 #define MILA_SUDOKU_SOLVER_PARALLEL_PROFILER_H_
 
+#include <chrono>
+#include <map>
+
+#include "sudoku_solver_parallel.h"
+
+namespace mila {
+namespace sudokusolver {
+namespace parallel {
+
+class SudokuSolverProfiler: public SudokuSolver {
+ public:
+  SudokuSolverProfiler();
+  SudokuSolverProfiler(size_t platform_id, size_t device_id);
+
+  std::vector<int> Run(const std::vector<int>& grid, int number_of_cells_to_fill) override;
+
+  std::string main_result() const;
+  std::map<std::string, int64_t> results() const;
+ private:
+  const std::string main_result_;
+  std::map<std::string, int64_t> results_;
+};
+
+}  // parallel
+}  // sudokusolver
+}  // mila
+
 #endif  // MILA_SUDOKU_SOLVER_PARALLEL_PROFILER_H_
