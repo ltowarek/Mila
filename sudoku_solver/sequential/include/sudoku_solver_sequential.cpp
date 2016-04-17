4524c9a55f40f4bcff1bb211a406c19bd4b791cc
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-04-17 14:41:00 +0200

    Remove std::find function

--- sudoku_solver/sequential/include/sudoku_solver_sequential.cpp
+++ sudoku_solver/sequential/include/sudoku_solver_sequential.cpp
@@ -4,15 +4,17 @@ bool mila::sudokusolver::sequential::SudokuSolver::IsValidHorizontally(const std
                                                                        int n,
                                                                        int row) const {
   auto is_valid = true;
-  auto checked_numbers = std::vector<int>();
+  auto checked_numbers = std::vector<int>(n, 0);
 
   for (auto column = 0; column < n; ++column) {
     auto value = grid[row * n + column];
-    if (value != 0 && std::find(checked_numbers.begin(), checked_numbers.end(), value) != checked_numbers.end()) {
-      is_valid = false;
-      break;
-    } else {
-      checked_numbers.push_back(value);
+    if (value != 0) {
+      if (checked_numbers[value - 1]) {
+        is_valid = false;
+        break;
+      } else {
+        checked_numbers[value - 1] = 1;
+      }
     }
   }
 
@@ -23,15 +25,17 @@ bool mila::sudokusolver::sequential::SudokuSolver::IsValidVertically(const std::
                                                                     int n,
                                                                     int column) const {
   auto is_valid = true;
-  auto checked_numbers = std::vector<int>();
+  auto checked_numbers = std::vector<int>(n, 0);
 
   for (auto row = 0; row < n; ++row) {
     auto value = grid[row * n + column];
-    if (value != 0 && std::find(checked_numbers.begin(), checked_numbers.end(), value) != checked_numbers.end()) {
-      is_valid = false;
-      break;
-    } else {
-      checked_numbers.push_back(value);
+    if (value != 0) {
+      if (checked_numbers[value - 1]) {
+        is_valid = false;
+        break;
+      } else {
+        checked_numbers[value - 1] = 1;
+      }
     }
   }
 
@@ -46,16 +50,18 @@ bool mila::sudokusolver::sequential::SudokuSolver::IsValidInBoxes(const std::vec
   const auto box_row = row / box_size;
   const auto box_column = column / box_size;
   auto is_valid = true;
-  auto checked_numbers = std::vector<int>();
+  auto checked_numbers = std::vector<int>(n, 0);
 
   for (auto tmp_row = box_row * box_size; tmp_row < box_row * box_size + box_size; ++tmp_row) {
     for (auto tmp_column = box_column * box_size; tmp_column < box_column * box_size + box_size; ++tmp_column) {
       auto value = grid[tmp_row * n + tmp_column];
-      if (value != 0 && std::find(checked_numbers.begin(), checked_numbers.end(), value) != checked_numbers.end()) {
-        is_valid = false;
-        break;
-      } else {
-        checked_numbers.push_back(value);
+      if (value != 0) {
+        if (checked_numbers[value - 1]) {
+          is_valid = false;
+          break;
+        } else {
+          checked_numbers[value - 1] = 1;
+        }
       }
     }
   }
