6a58c30e7a01243faa4b8c07c4cc865e93fefafc
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-04-27 19:39:05 +0200

    Add Particle structure

--- n_body/parallel/include/n_body_parallel.h
+++ n_body/parallel/include/n_body_parallel.h
@@ -1,4 +1,20 @@
 #ifndef MILA_N_BODY_PARALLEL_H_
 #define MILA_N_BODY_PARALLEL_H_
 
+#include "clpp.h"
+
+namespace mila {
+namespace nbody {
+namespace parallel {
+
+struct Particle {
+  cl_float2 position;
+  cl_float2 velocity;
+  cl_float2 acceleration;
+};
+
+}  // parallel
+}  // nbody
+}  // mila
+
 #endif  // MILA_N_BODY_PARALLEL_H_
---
+++ n_body/parallel/test/n_body_parallel_test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "n_body_parallel.h"
+#include "n_body_parallel_profiler.h"
+
+TEST(NBodyParticleTest, InitializeWithSingleValue) {
+  mila::nbody::parallel::Particle particle = mila::nbody::parallel::Particle{{0.0f}};
+  EXPECT_EQ(particle.position.x, 0.0f);
+  EXPECT_EQ(particle.position.y, 0.0f);
+  EXPECT_EQ(particle.velocity.x, 0.0f);
+  EXPECT_EQ(particle.velocity.y, 0.0f);
+  EXPECT_EQ(particle.acceleration.x, 0.0f);
+  EXPECT_EQ(particle.acceleration.y, 0.0f);
+}
+
+TEST(NBodyParticleTest, InitializeWithMultipleValues) {
+  mila::nbody::parallel::Particle particle = {{1.0f, 2.0f}, {3.0f, 4.0f}, {5.0f, 6.0f}};
+  EXPECT_EQ(particle.position.x, 1.0f);
+  EXPECT_EQ(particle.position.y, 2.0f);
+  EXPECT_EQ(particle.velocity.x, 3.0f);
+  EXPECT_EQ(particle.velocity.y, 4.0f);
+  EXPECT_EQ(particle.acceleration.x, 5.0f);
+  EXPECT_EQ(particle.acceleration.y, 6.0f);
+}
