14799b4c97f56aa31390d27750780357770e56fc
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-04-27 14:00:41 +0200

    Change random seed to 0

--- n_body/utils/include/n_body_utils.cpp
+++ n_body/utils/include/n_body_utils.cpp
@@ -1,8 +1,8 @@
 #include "n_body_utils.h"
 
 float mila::nbody::utils::GenerateRandomValue(float min, float max) {
-  static std::random_device device;
-  static std::mt19937 generator(device());
+  // Constant seed provides stable results
+  static std::mt19937 generator(0);
   static std::uniform_real_distribution<float> distribution(min, max);
   return distribution(generator);
 }
