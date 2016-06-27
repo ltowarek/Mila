91211652ef92c736a45153b62f580a1e511f6a76
ltowarek <lukasz.towarek@gmail.com>
2016-06-27 15:09:12 +0200

    Fix indentation

--- n_body/parallel/main.cpp
+++ n_body/parallel/main.cpp
@@ -16,47 +16,51 @@ struct parameters {
 };
 
 parameters ParseCommandLine(int argc, char **argv) {
-    auto config = parameters{};
-    // TODO: Verify input and add help message
-    config.input_file = std::string(argv[1]);
-    config.number_of_particles = atoi(argv[2]);
-    config.platform_id = static_cast<size_t>(atoi(argv[3]));
-    config.device_id = static_cast<size_t>(atoi(argv[4]));
-    config.number_of_iterations = static_cast<size_t>(atoi(argv[5]));
-    return config;
+  auto config = parameters{};
+  // TODO: Verify input and add help message
+  config.input_file = std::string(argv[1]);
+  config.number_of_particles = atoi(argv[2]);
+  config.platform_id = static_cast<size_t>(atoi(argv[3]));
+  config.device_id = static_cast<size_t>(atoi(argv[4]));
+  config.number_of_iterations = static_cast<size_t>(atoi(argv[5]));
+  return config;
 }
 
 int main(int argc, char **argv) {
-    auto config = ParseCommandLine(argc, argv);
-    printf("%s\n", mila::version::PrintVersion().c_str());
+  auto config = ParseCommandLine(argc, argv);
+  printf("%s\n", mila::version::PrintVersion().c_str());
 
-    auto n_body_initial = mila::nbody::parallel::NBodyParallelWithInputFileProfiler(config.number_of_particles, config.platform_id, config.device_id);
-    n_body_initial.Run(config.input_file);
-    auto duration = n_body_initial.results().at(n_body_initial.main_result());
-    printf("Initial results\n");
-    printf("Duration [us]: %lld\n", duration);
-    printf("Platform: %s\n", n_body_initial.platform().getName().c_str());
-    printf("Device: %s\n", n_body_initial.device().getName().c_str());
-    printf("Input file: %s\n", config.input_file.c_str());
-    printf("Number of particles: %d\n", config.number_of_particles);
+  auto n_body_initial = mila::nbody::parallel::NBodyParallelWithInputFileProfiler(config.number_of_particles,
+                                                                                  config.platform_id,
+                                                                                  config.device_id);
+  n_body_initial.Run(config.input_file);
+  auto duration = n_body_initial.results().at(n_body_initial.main_result());
+  printf("Initial results\n");
+  printf("Duration [us]: %lld\n", duration);
+  printf("Platform: %s\n", n_body_initial.platform().getName().c_str());
+  printf("Device: %s\n", n_body_initial.device().getName().c_str());
+  printf("Input file: %s\n", config.input_file.c_str());
+  printf("Number of particles: %d\n", config.number_of_particles);
 
-    auto results = std::vector<float>(config.number_of_iterations);
-    printf("Iterations\n");
-    for (size_t i = 0; i < config.number_of_iterations; ++i) {
-        auto n_body = mila::nbody::parallel::NBodyParallelWithInputFileProfiler(config.number_of_particles, config.platform_id, config.device_id);
-        n_body.Run(config.input_file);
-        duration = n_body.results().at(n_body.main_result());
-        printf("Iteration: %d, Duration [us]: %lld\n", i, duration);
-        results[i] = duration;
-    }
+  auto results = std::vector<float>(config.number_of_iterations);
+  printf("Iterations\n");
+  for (size_t i = 0; i < config.number_of_iterations; ++i) {
+    auto n_body = mila::nbody::parallel::NBodyParallelWithInputFileProfiler(config.number_of_particles,
+                                                                            config.platform_id,
+                                                                            config.device_id);
+    n_body.Run(config.input_file);
+    duration = n_body.results().at(n_body.main_result());
+    printf("Iteration: %d, Duration [us]: %lld\n", i, duration);
+    results[i] = duration;
+  }
 
-    printf("Statistics\n");
-    printf("Mean: %f\n", mila::utils::Mean(results));
-    printf("Median: %f\n", mila::utils::Median(results));
-    printf("Variance: %f\n", mila::utils::Variance(results));
-    printf("Standard Deviation: %f\n", mila::utils::StandardDeviation(results));
-    printf("Coefficient of Variation: %f\n", mila::utils::CoefficientOfVariation(results));
+  printf("Statistics\n");
+  printf("Mean: %f\n", mila::utils::Mean(results));
+  printf("Median: %f\n", mila::utils::Median(results));
+  printf("Variance: %f\n", mila::utils::Variance(results));
+  printf("Standard Deviation: %f\n", mila::utils::StandardDeviation(results));
+  printf("Coefficient of Variation: %f\n", mila::utils::CoefficientOfVariation(results));
 
-    return 0;
+  return 0;
 }
 
--- sudoku_solver/sequential/main.cpp
+++ sudoku_solver/sequential/main.cpp
@@ -9,42 +9,42 @@ struct parameters {
 };
 
 parameters ParseCommandLine(int argc, char **argv) {
-    auto config = parameters{};
-    // TODO: Verify input and add help message
-    config.input_file = std::string(argv[1]);
-    config.output_file = std::string(argv[2]);
-    config.number_of_iterations = static_cast<size_t>(atoi(argv[3]));
-    return config;
+  auto config = parameters{};
+  // TODO: Verify input and add help message
+  config.input_file = std::string(argv[1]);
+  config.output_file = std::string(argv[2]);
+  config.number_of_iterations = static_cast<size_t>(atoi(argv[3]));
+  return config;
 }
 
 int main(int argc, char **argv) {
-    auto config = ParseCommandLine(argc, argv);
-    printf("%s\n", mila::version::PrintVersion().c_str());
+  auto config = ParseCommandLine(argc, argv);
+  printf("%s\n", mila::version::PrintVersion().c_str());
 
-    auto sudoku_solver_initial = mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler();
-    sudoku_solver_initial.Run(config.input_file, config.output_file);
-    auto duration = sudoku_solver_initial.results().at(sudoku_solver_initial.main_result());
-    printf("Initial results\n");
-    printf("Duration [us]: %lld\n", duration);
-    printf("Input file: %s\n", config.input_file.c_str());
-    printf("Output file: %s\n", config.output_file.c_str());
+  auto sudoku_solver_initial = mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler();
+  sudoku_solver_initial.Run(config.input_file, config.output_file);
+  auto duration = sudoku_solver_initial.results().at(sudoku_solver_initial.main_result());
+  printf("Initial results\n");
+  printf("Duration [us]: %lld\n", duration);
+  printf("Input file: %s\n", config.input_file.c_str());
+  printf("Output file: %s\n", config.output_file.c_str());
 
-    auto results = std::vector<float>(config.number_of_iterations);
-    printf("Iterations\n");
-    for (size_t i = 0; i < config.number_of_iterations; ++i) {
-        auto sudoku_solver = mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler();
-        sudoku_solver.Run(config.input_file, config.output_file);
-        duration = sudoku_solver.results().at(sudoku_solver.main_result());
-        printf("Iteration: %d, Duration [us]: %lld\n", i, duration);
-        results[i] = duration;
-    }
+  auto results = std::vector<float>(config.number_of_iterations);
+  printf("Iterations\n");
+  for (size_t i = 0; i < config.number_of_iterations; ++i) {
+    auto sudoku_solver = mila::sudokusolver::sequential::SudokuSolverBasedOnFilesProfiler();
+    sudoku_solver.Run(config.input_file, config.output_file);
+    duration = sudoku_solver.results().at(sudoku_solver.main_result());
+    printf("Iteration: %d, Duration [us]: %lld\n", i, duration);
+    results[i] = duration;
+  }
 
-    printf("Statistics\n");
-    printf("Mean: %f\n", mila::utils::Mean(results));
-    printf("Median: %f\n", mila::utils::Median(results));
-    printf("Variance: %f\n", mila::utils::Variance(results));
-    printf("Standard Deviation: %f\n", mila::utils::StandardDeviation(results));
-    printf("Coefficient of Variation: %f\n", mila::utils::CoefficientOfVariation(results));
+  printf("Statistics\n");
+  printf("Mean: %f\n", mila::utils::Mean(results));
+  printf("Median: %f\n", mila::utils::Median(results));
+  printf("Variance: %f\n", mila::utils::Variance(results));
+  printf("Standard Deviation: %f\n", mila::utils::StandardDeviation(results));
+  printf("Coefficient of Variation: %f\n", mila::utils::CoefficientOfVariation(results));
 
-    return 0;
+  return 0;
 }
