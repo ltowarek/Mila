582e580e03ee15d0716fa691bafa6246646d8cd3
ltowarek <lukasz.towarek@gmail.com>
2016-07-17 19:55:39 +0200

    Move OpenCLStatistics class from bbp_parallel to bbp_parallel_profiler

--- bbp/parallel/include/bbp_parallel.h
+++ bbp/parallel/include/bbp_parallel.h
@@ -16,61 +16,6 @@ namespace mila {
 namespace bbp {
 namespace parallel {
 
-class OpenCLStatistics {
- public:
-  inline size_t GetBuildKernelAsMicroseconds() {
-    return static_cast<size_t>(build_kernel_.count());
-  }
-
-  inline void SetBuildKernelAsMicroseconds(size_t microseconds) {
-    build_kernel_ = std::chrono::microseconds(microseconds);
-  }
-
-  inline std::string GetBuildKernelAsString() {
-    return GetStatisticsAsString("Build kernel", static_cast<size_t>(build_kernel_.count()));
-  }
-
-  inline size_t GetReadBufferAsMicroseconds() {
-    return static_cast<size_t>(read_buffer_.count());
-  }
-
-  inline void SetReadBufferAsMicroseconds(size_t microseconds) {
-    read_buffer_ = std::chrono::microseconds(microseconds);
-  }
-
-  inline std::string GetReadBufferAsString() {
-    return GetStatisticsAsString("Read buffer", static_cast<size_t>(read_buffer_.count()));
-  }
-
-  inline size_t GetEnqueueNDRangeAsMicroseconds() {
-    return static_cast<size_t>(enqueue_nd_range_.count());
-  }
-
-  inline void SetEnqueueNDRangeAsMicroseconds(size_t microseconds) {
-    enqueue_nd_range_ = std::chrono::microseconds(microseconds);
-  }
-
-  inline std::string GetEnqueueNDRangeAsString() {
-    return GetStatisticsAsString("Enqueue ND range", static_cast<size_t>(enqueue_nd_range_.count()));
-  }
-
-  inline std::string GetOpenCLStatisticsAsString() {
-    std::stringstream stream;
-    stream << GetBuildKernelAsString() << ", " << GetReadBufferAsString() << ", " << GetEnqueueNDRangeAsString();
-    return stream.str();
-  }
- private:
-  inline std::string GetStatisticsAsString(const std::string& name, size_t value) {
-    std::stringstream stream;
-    stream << name << ": " << value << " us";
-    return stream.str();
-  }
-
-  std::chrono::microseconds build_kernel_ = std::chrono::microseconds(0);
-  std::chrono::microseconds enqueue_nd_range_ = std::chrono::microseconds(0);
-  std::chrono::microseconds read_buffer_ = std::chrono::microseconds(0);
-};
-
 class BBP {
  public:
   BBP();
