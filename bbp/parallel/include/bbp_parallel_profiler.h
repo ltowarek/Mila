12aea7ddb4fb77fc3151e5d4e9460caf34a2053e
ltowarek <lukasz.towarek@gmail.com>
2016-08-14 23:22:54 +0200

    Remove unused ProfileFunction function

--- bbp/parallel/include/bbp_parallel_profiler.h
+++ bbp/parallel/include/bbp_parallel_profiler.h
@@ -47,14 +47,6 @@ class BBPFactory {
                           std::unique_ptr<mila::Logger> logger);
 };
 
-template<typename F, typename... A>
-std::chrono::duration<float> ProfileFunction(F &&f, A &&... a) {
-  auto start_time = std::chrono::high_resolution_clock::now();
-  f(std::forward<A>(a)...);
-  auto end_time = std::chrono::high_resolution_clock::now();
-  return std::chrono::duration<float>(end_time - start_time);
-};
-
 class OpenCLApplicationProfiler: public OpenCLApplication {
  public:
   OpenCLApplicationProfiler();
