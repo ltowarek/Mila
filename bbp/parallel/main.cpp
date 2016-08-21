d26f78f2a385cc373b50be2af352434878b104fe
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-08-21 21:55:49 +0200

    Print basic info in parallel bbp app

--- bbp/parallel/main.cpp
+++ bbp/parallel/main.cpp
@@ -1,4 +1,16 @@
+#include <memory>
+
+#include "logger.h"
+#include "ocl_app.h"
+#include "version.h"
+
 int main(int argc, char **argv) {
+  auto logger = std::shared_ptr<mila::Logger>(mila::LoggerFactory().MakePrintf());
+  logger->Info("Version: %s", mila::version::PrintVersion().c_str());
+  auto ocl_app = mila::OpenCLApplicationFactory().MakeGeneric(0, 0, logger);
+  logger->Info("Platform name: %s", ocl_app->GetPlatformName().c_str());
+  logger->Info("Device name: %s", ocl_app->GetDeviceName().c_str());
+
   return 0;
 }
 
