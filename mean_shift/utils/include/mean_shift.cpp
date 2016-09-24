bce49914dcdae744680d9c882c2346121d1dd1c6
Lukasz Towarek <lukasz.towarek@gmail.com>
2016-09-24 12:01:14 +0200

    Create interfaces for mean shift

---
+++ mean_shift/utils/include/mean_shift.cpp
@@ -0,0 +1,13 @@
+#include "mean_shift.h"
+
+mila::MeanShift::~MeanShift() {
+
+}
+
+mila::MeanShiftProfiler::~MeanShiftProfiler() {
+
+}
+
+mila::MeanShiftApp::~MeanShiftApp() {
+
+}
---
+++ mean_shift/utils/include/mean_shift.h
@@ -0,0 +1,32 @@
+#ifndef MILA_UTILS_MEAN_SHIFT_H_
+#define MILA_UTILS_MEAN_SHIFT_H_
+
+#include <vector>
+
+namespace mila {
+struct Point {
+  float x;
+  float y;
+  float z;
+  float w;
+};
+
+class MeanShift {
+ public:
+  virtual ~MeanShift() = 0;
+  virtual std::vector<Point> Run(const std::vector<Point> &points, float bandwidth) = 0;
+};
+
+class MeanShiftProfiler : public MeanShift {
+ public:
+  virtual ~MeanShiftProfiler() = 0;
+};
+
+class MeanShiftApp {
+ public:
+  virtual ~MeanShiftApp() = 0;
+  virtual void Run(int argc, char **argv) const = 0;
+};
+}  // mila
+
+#endif  // MILA_UTILS_MEAN_SHIFT_H_
